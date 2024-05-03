44f5e48c0cec2e175cc8283280220d280783d459
Tiago <rm93416@estudante.fieb.org.br>
2024-05-03 20:28:03 -0300

    Versão inicial

---
+++ src/main/java/br/com/applip1/novocrudrest/NovocrudrestApplication.java
@@ -0,0 +1,13 @@
+package br.com.applip1.novocrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovocrudrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovocrudrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/applip1/novocrudrest/NovocrudrestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.applip1.novocrudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovocrudrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
